4945477b0bab79fa3b6265cd4b9ae4c0089c8edd
Oanh <oanhvo221199@gmail.com>
2025-05-14 06:40:25 +0700

    update sychronized to atomic

--- multithread-example/src/com/company/models/Account.java
+++ multithread-example/src/com/company/models/Account.java
@@ -1,33 +1,44 @@
 package com.company.models;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Account {
-    private  Integer balance;
+    // Use Atomic Interger ( Atomic) thay Integer balance(synchronized)
+    private AtomicInteger balance;
 
     public Account(Integer balance) {
-        this.balance = balance;
+        this.balance = new AtomicInteger(balance);
     }
 
     public Integer getBalance() {
-        return balance;
+        return balance.get();
     }
 
     public void setBalance(Integer balance) {
-        this.balance = balance;
+        this.balance.set(balance);
     }
 
-    public synchronized  void deposit(Integer amount, User user){
-        this.balance += amount;
-        System.out.println( user.toString() + "  deposited:  " + amount + "|" + "balance: " + this.balance);
+    // delete synchronized
+    public void deposit(Integer amount, User user){
+        // Use AtomicInteger addAndGet change +
+        int newBalance = balance.addAndGet(amount);
+        System.out.println(user.toString() + " deposited: " + amount + " | balance: " + newBalance);
     }
 
-    public synchronized  boolean withdraw(Integer amount, User user){
-        if (amount> this.balance){
-            System.out.println("You do not have enough balance to withdraw this amount"+ "|" + "balance: " + this.balance);
-            return  false;
+    public boolean withdraw(Integer amount, User user){
+        while (true) {
+            // Use AtomicInteger get
+            int current = balance.get();
+            if (amount > current) {
+                System.out.println("You do not have enough balance to withdraw this amount | balance: " + current);
+                return false;
+            }
+            // Use AtomicInteger compareAndSet change compare amount
+            boolean updated = balance.compareAndSet(current, current - amount);
+            if (updated) {
+                System.out.println(user.toString() + " withdraw: " + amount + " | balance: " + (current - amount));
+                return true;
+            }
         }
-        this.balance -= amount;
-        System.out.println( user.toString() + "  withdraw:  " + amount + "|" + "balance: " + this.balance);
-        return  true;
     }
-
 }
